96c8d3e3f0e00fe24e894e57584a4915145ffea9
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-04-20 14:51:17 -0700

    refactored test under one 'describe'

--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -2,7 +2,7 @@ import { expect, describe, it } from "vitest";
 import { Item, items, updateQuality } from "./gilded-rose.js";
 
 describe("updateQuality", () => {
-    it("Reduces quality and sellIn of 'basic' items by 1", () => {
+    it("Reduces quality and sellIn of 'basic' items by 1.", () => {
         const testItem = new Item("basic", 5, 3);
         items.push(testItem);
 
@@ -11,10 +11,8 @@ describe("updateQuality", () => {
         expect(testItem.sellIn).toBe(4);
         expect(testItem.quality).toBe(2);
     });
-});
 
-describe("updateQuality", () => {
-    it("Once sellIn is less than 0 the quality degrades twice as fast", () => {
+    it("Once sellIn is less than 0 the quality degrades twice as fast.", () => {
         const testItem = new Item("basic", 0, 10);
         items.push(testItem);
 
@@ -23,10 +21,8 @@ describe("updateQuality", () => {
         expect(testItem.sellIn).toBe(-1);
         expect(testItem.quality).toBe(8);
     });
-});
 
-describe("updateQuality", () => {
-    it("Quality is never negative", () => {
+    it("Quality is never negative.", () => {
         const testItem = new Item("basic", 5, 0);
         items.push(testItem);
 
@@ -35,10 +31,8 @@ describe("updateQuality", () => {
         expect(testItem.sellIn).toBe(4);
         expect(testItem.quality).toBe(0);
     });
-});
 
-describe("updateQuality", () => {
-    it("'Aged Brie' quility increases the older it gets", () => {
+    it("'Aged Brie' quility increases the older it gets.", () => {
         const testItem = new Item("Aged Brie", 2, 0);
         items.push(testItem);
 
@@ -47,10 +41,8 @@ describe("updateQuality", () => {
         expect(testItem.sellIn).toBe(1);
         expect(testItem.quality).toBe(1);
     });
-});
 
-describe("updateQuality", () => {
-    it("Quality and sellIn of 'Sulfras' does not change", () => {
+    it("Quality and sellIn of 'Sulfras' does not change.", () => {
         const testItem = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
         items.push(testItem);
     
@@ -59,10 +51,8 @@ describe("updateQuality", () => {
         expect(testItem.sellIn).toBe(0);
         expect(testItem.quality).toBe(80);
     });
-});
 
-describe("updateQuality", () => {
-    it("'Backstage passes' quality increase as sellIn decrease", () => {
+    it("'Backstage passes' quality increase as sellIn decrease.", () => {
         const testItem = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
         items.push(testItem);
     
@@ -71,10 +61,8 @@ describe("updateQuality", () => {
         expect(testItem.sellIn).toBe(14);
         expect(testItem.quality).toBe(21);
     });
-});
 
-describe("updateQuality", () => {
-    it("'Backstage passes' quality increase by 2 when sellIn is less than 10", () => {
+    it("'Backstage passes' quality increase by 2 when sellIn is less than 10.", () => {
         const testItem = new Item("Backstage passes to a TAFKAL80ETC concert", 9, 20);
         items.push(testItem);
     
@@ -83,10 +71,8 @@ describe("updateQuality", () => {
         expect(testItem.sellIn).toBe(8);
         expect(testItem.quality).toBe(22);
     });
-});
 
-describe("updateQuality", () => {
-    it("'Backstage passes' quality increase by 3 when sellIn is less than 5", () => {
+    it("'Backstage passes' quality increase by 3 when sellIn is less than 5.", () => {
         const testItem = new Item("Backstage passes to a TAFKAL80ETC concert", 4, 20);
         items.push(testItem);
     
@@ -95,10 +81,8 @@ describe("updateQuality", () => {
         expect(testItem.sellIn).toBe(3);
         expect(testItem.quality).toBe(23);
     });
-});
 
-describe("updateQuality", () => {
-    it("'Backstage passes' quality drops to 0 when sellIn reaches 0", () => {
+    it("'Backstage passes' quality drops to 0 when sellIn reaches 0.", () => {
         const testItem = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
         items.push(testItem);
     
